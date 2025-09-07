fe9295555839e1f9b90682dd3ddcb303d6ac2955
Ahmed Refaat Mohamed <ahmedrefaat532@aucegypt.edu>
2025-09-08 00:20:43 +0300

    Proper license and attribution

--- run-clang-tidy.sh
+++ run-clang-tidy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# ZDB a distributed, fault-tolerant database.
+# Copyright (C) 2025 Ahmed Refaat Gadalla Mohamed
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 # Script to run clang-tidy on the ZDB project
 # Usage: ./run-clang-tidy.sh [build_directory] [output_format]
 #   output_format: html (default), yaml, both
