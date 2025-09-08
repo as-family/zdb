4c8173bc7b15c2bf8de092bdc7d54e4aac3ec419
Ahmed Refaat Mohamed <ahmedrefaat532@aucegypt.edu>
2025-09-08 14:54:25 +0300

    Add proper license and attribution

--- tst/KVTestFramework/ProxyKVStoreService.hpp
+++ tst/KVTestFramework/ProxyKVStoreService.hpp
@@ -1,3 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+/*
+ * ZDB a distributed, fault-tolerant database.
+ * Copyright (C) 2025 Ahmed Refaat Gadalla Mohamed
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef PROXY_KV_STORE_SERVICE_H
 #define PROXY_KV_STORE_SERVICE_H
 
